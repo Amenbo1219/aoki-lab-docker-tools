9880a55310fe677810d7c9a01b39884574b31763
Amembo <68372605+Amenbo1219@users.noreply.github.com>
2025-01-29 20:34:06 +0900

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,33 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
 # Lingerを有効化
-loginctl enable-linger "$USER"
+loginctl enable-linger $USER
 
 # XDG_RUNTIME_DIRを設定
 export XDG_RUNTIME_DIR="/run/user/$UID"
 
-# 必要なディレクトリを作成
+# 必要なディレクトリを作成（存在しない場合）
 if [ ! -d "$XDG_RUNTIME_DIR" ]; then
     mkdir -p "$XDG_RUNTIME_DIR"
     chmod 700 "$XDG_RUNTIME_DIR"
     chown "$USER:$USER" "$XDG_RUNTIME_DIR"
 fi
 
-# dockerd-rootless-setuptoolの実行
-bash dockerd-rootless-setuptool.sh install
+# dockerd-rootless-setuptool.sh の実行
+dockerd-rootless-setuptool.sh install
 
 # PATHとDOCKER_HOSTを設定
 export PATH="/usr/bin:$PATH"
 export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/docker.sock"
 
 # Dockerサービスを設定
-systemctl --user enable docker
-systemctl --user start docker
-systemctl --user restart docker
+systemctl --user enable docker.service
+systemctl --user start docker.service
+systemctl --user restart docker.service
 
 mkdir -p ~/.config/docker
 echo "{\"data-root\":\"/docker-cache/$USER/\"}" > ~/.config/docker/daemon.json
 mkdir -p /docker-cache/$USER/
 sleep 1
 systemctl --user restart docker
-
