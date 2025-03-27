42111ea1084a555ed3e03694d9cefc4a415c2234
Amembo <68372605+Amenbo1219@users.noreply.github.com>
2025-03-27 12:11:25 +0900

    Update alldel.sh

--- alldel.sh
+++ alldel.sh
@@ -1,4 +1,7 @@
 dockerstart.sh
+docker system prune -a -f
+docker container prune -a -f
 docker image prune -a -f
-docker container prune -f
+docker volume prune -f
+docker network prune -f
 dockerstop.sh
